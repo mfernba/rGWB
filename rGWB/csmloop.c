0995a38e5a6f6dee56030fcdda21129872da1720
Manuel <manuel.aws@outlook.es>
2018-05-22 18:57:55 +0200

    Memory leaks

--- rGWB/csmloop.c
+++ rGWB/csmloop.c
@@ -20,6 +20,7 @@
 #else
 #include "cyassert.h"
 #include "cypespy.h"
+#include "cypespy.inl"
 #endif
 
 // --------------------------------------------------------------------------------------------------------------
@@ -32,7 +33,7 @@ static void i_csmloop_destruye(struct csmloop_t **loop)
     if ((*loop)->ledge != NULL)
         csmnode_free_node_list(&(*loop)->ledge, csmhedge_t);
     
-    FREE_PP(loop, struct csmloop_t);
+    FREE_PP_NO_CLEAN_MEMORY(loop, struct csmloop_t);
 }
 
 // --------------------------------------------------------------------------------------------------------------
